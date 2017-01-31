55be535d129dd1332a7a44eab2db7eedf3eb9909
melbandit <melissa.i.sattler@gmail.com>
2017-01-30 23:49:31 -0500

    last edit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,84 @@
-function myFunction(event) {
-    var x = event.value;
-    document.querySelector("parent-pwd").innerHTML = x;
+var levelselect = $('.level-select'),
+	currentSelect = $('.level-select.current');
+
+levelselect.on("click", function (e){
+	//	e.preventDefault();
+
+	var clickLevel = $(this);
+
+	if (clickLevel.hasClass('current')){
+	}else{
+
+		levelselect.removeClass('current');
+		levelselect.attr('src', 'assets/img/parents/unselected.png');
+		clickLevel.addClass('current');
+		clickLevel.attr('src', 'assets/img/parents/selected.png');
+	}
+});
+
+
+(function () {
+	// grab variables all my global variable
+	var numbuttons = $('.parent-num'),
+		pwdinput = $('#parent-pwd'),
+		msg = "";
+		
+	// when a number button is clicked
+	numbuttons.on('click', function (evt) {
+		evt.preventDefault();
+
+		var clickVal = $(this).val(),
+			val = pwdinput.val(),
+			realVal = val + clickVal;
+
+		console.log(clickVal);
+
+		pwdinput.val( pwdinput.val()+clickVal );
+
+		if (pwdinput.val() == "112") {
+			//go to next page
+			console.log("go to next page");
+			window.location.href = "parentHome.html";
+		} else if (pwdinput.val().length > 2) {
+			setTimeout(function() {
+				pwdinput.val("");
+			}, 200);
+		}
+	});
+})();
+
+// var $color = $('.color');
+// // Delay tween by a random number between 1 & 2
+// TweenMax.to($color, 1, {x:'85', delay: delay});
+
+var left = getRandom(700, 710);
+var right = getRandom(820, 830);
+var delay = getRandom(1, 2.5);
+//basic illustration of TweenMax's repeat, repeatDelay, yoyo and onRepeat
+//var color = document.querySelectorAll('.color');
+function getRandom(min, max) {
+	return min + Math.random() * (max - min);
 }
-// <input type="text" onkeydown="myFunction(event)"> 
 
-// function myFunction(event) {
-//     var x = event.keyCode;
-//     if (x == 112) {  // 27 is the ESC key
-//         alert ("You pressed the Escape key!");
-//     }
-// }
-// $('#one').val('1');
-// $('#two').val('2');
-// $('#three').val('3');
-// $('#four').val('4');
-// $('#five').val('5');
-// $('#six').val('6');
-// $('#seven').val('7');
-// $('#eight').val('8');
-// $('#nine').val('9');
+TweenMax.to(".color-one", 1, {left:left+"px", yoyo:true, delay: 2, repeat:40, repeatDelay: 2, ease:Linear.easeNone});
+TweenMax.to(".color-two", 1, {left:left+"px", yoyo:true, delay: 2, repeat:40, repeatDelay: 2, ease:Linear.easeNone});
+TweenMax.to(".color-three", 1, {left:left+"px", yoyo:true, delay: 3, repeat:40, repeatDelay: 3, ease:Linear.easeNone});
+TweenMax.to(".color-four", 1, {left:left+"px", yoyo:true, delay: 3, repeat:40, repeatDelay: 3, ease:Linear.easeNone});
+TweenMax.to(".color-five", 1.5, {left:left+"px", yoyo:true, delay: delay, repeat:40, repeatDelay: 4, ease:Linear.easeNone});
+TweenMax.to(".color-six", 1.5, {left:left+"px", yoyo:true, delay: 4, repeat:40, repeatDelay: 4, ease:Linear.easeNone});
+TweenMax.to(".color-seven", 1.5, {left:right+"px", yoyo:true, delay: 5, repeat:40, repeatDelay: 5, ease:Linear.easeNone});
+TweenMax.to(".color-eight", 1.5, {left:right+"px", yoyo:true, delay: 5, repeat:40, repeatDelay: 5, ease:Linear.easeNone});
+TweenMax.to(".color-eight", 2, {left:right+"px", yoyo:true, delay: 1, repeat:40, repeatDelay: 1, ease:Linear.easeNone});
+TweenMax.to(".color-nine", 2, {left:right+"px", yoyo:true, delay: 1, repeat:40, repeatDelay: 1, ease:Linear.easeNone});
+TweenMax.to(".color-ten", 2, {left:right+"px", yoyo:true, delay: delay, repeat:40, repeatDelay: delay, ease:Linear.easeNone});
 
+
+
+// function onRepeat() {
+//   count++;
+//   box.innerHTML = count;
+//   TweenLite.set(box, {backgroundColor:"hsl(" + Math.random() * 255 + ", 90%, 60%)"});
+// }  
 //drag and drop
 // $( function() {
 // 	$( ".draggable" ).draggable();

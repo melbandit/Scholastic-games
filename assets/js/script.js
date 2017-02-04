3b218166ec5d2f9a7b1eb8daa397164c5087b3c0
melbandit <melissa.i.sattler@gmail.com>
2017-02-04 16:18:46 -0500

    most recent updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,20 @@
+//sessionStorage.clear();
+// localStorage.clear();
+
 // Start of local storage
 //window.onload = function() {
 	youWinValCheck();
-	console.log(youWinVal);
+    console.log(output);
+	
 //}
 //End of local storage
-var youWinVal = 0;
-var $link_two = document.querySelector("link-two");
-var $link_three = document.querySelector("link-three");
-var $link_four = document.querySelector("link-four");
+var output;
+var youWinVal;
+var $link_two = document.querySelector(".link-two");
+var $link_three = document.querySelector(".link-three");
+var $link_four = document.querySelector(".link-four");
 var $youWin_bkg = document.querySelector(".you-win-bkg");
-
+//console.log(youWinVal);
 // function youWinVal(){
 //     console.log("working");
 //     if(youWinVal === 1){
@@ -25,47 +30,51 @@ var $youWin_bkg = document.querySelector(".you-win-bkg");
 //         $link_two.setAttribute("class", "link-four");
 //     }
 // }
+function OutputNumber(){
+    var newWinVal = youWinVal + 1;
+    sessionStorage.setItem('NewWinVal', newWinVal);
+}
 function youWinLevelOne(){
-    console.log("You Win!!");
-    //youWinVal = 1;
-    localStorage.setItem('youWinVal', 1);
+    console.log("You Win Lovel One!!");
+    var youWinVal = 1;
+    OutputNumber();
+    var output = sessionStorage.getItem("NewWinVal");
     $youWin_bkg.setAttribute("class", "you-win-bkg");
-    
-    youWinValCheck();
-    console.log(youWinVal);
+     youWinValCheck();
+    //console.log(youWinVal);
     // youWinVal();
 }
 function youWinLevelTwo(){
     console.log("You Win!!");
-    //localStorage.setItem('key', JSON.stringify(Math.random()));
+    //sessionStorage.setItem('key', JSON.stringify(Math.random()));
     //youWinVal = 2;
-    localStorage.setItem('youWinVal', 2);
+    sessionStorage.setItem('youWinVal', 2);
     $youWin_bkg.setAttribute("class", "you-win-bkg");
     console.log(youWinVal);
-    youWinValCheck();
+    // youWinValCheck();
     // youWinVal();
 }
 function youWinLevelThree(){
     console.log("You Win!!");
     //youWinVal = 3;
-    localStorage.setItem('youWinVal', 3);
+    sessionStorage.setItem('youWinVal', 3);
     $youWin_bkg.setAttribute("class", "you-win-bkg");
     console.log(youWinVal);
-    youWinValCheck();
+    // youWinValCheck();
     // youWinVal();
 }
 function youWinValCheck(){
-    console.log("working");
-    if(youWinVal === 1){
+    //console.log("working");
+    if(output === 2){
         console.log("youWinVal worked");
         $link_two.setAttribute("class", "link-two");
     }
-    if(youWinVal === 2){
+    if(output === 3){
         console.log("youWinVal worked");
-        $link_two.setAttribute("class", "link-three");
+        $link_three.setAttribute("class", "link-three");
     }
-    if(youWinVal === 3){
+    if(output === 4){
         console.log("youWinVal worked");
-        $link_two.setAttribute("class", "link-four");
+        $link_three.setAttribute("class", "link-four");
     }
 }

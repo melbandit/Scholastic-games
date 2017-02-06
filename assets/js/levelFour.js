c4b4682f46112cd501ac08a74db8badb7b7a2adf
melbandit <melissa.i.sattler@gmail.com>
2017-02-06 13:35:05 -0500

    updated parent page

--- assets/js/levelFour.js
+++ assets/js/levelFour.js
@@ -2,7 +2,7 @@ $( function() {
 	$( "#sortable" ).sortable();
 	$( "#sortable" ).disableSelection();
 } );
-
+//localStorage.clear();
 var $star_one = document.querySelector(".star-one");
 var $star_two = document.querySelector(".star-two");
 var $star_three = document.querySelector(".star-three");
@@ -16,7 +16,6 @@ var $num_four = document.querySelector(".number-four");
 var $num_five = document.querySelector(".number-five");
 
 var $youWin_bkg = document.querySelector(".you-win-bkg");
-
 // $('#iggy-char').sprite({fps: 4, no_of_frames: 7, play_frames: 14});
 // $('#purple-char').sprite({fps: 4, no_of_frames: 5, play_frames: 14});
 // $('#neba-char').sprite({fps: 4, no_of_frames: 5, play_frames: 14});
@@ -29,62 +28,95 @@ $('#neba-char-2').sprite({fps: 4, no_of_frames: 5});
 function youWin(){
     $youWin_bkg.setAttribute("class", "you-win-bkg");
 }
-$("ul li").mouseup(function(){
-	if ($( "ul li" ).first().hasClass('three')){
-		TweenMax.fromTo(".star-one", 1, {scale:3}, {scale:1});
-		$num_three.setAttribute("class", "invisible");
-		$num_five.setAttribute("class", "number-five");
-		numTwo();
-		$('#iggy-char-lv4').sprite({fps: 4, no_of_frames: 7, play_frames: 14});
-		$('#purple-char-lv4').sprite({fps: 4, no_of_frames: 5, play_frames: 14});
-		$('#neba-char-lv4').sprite({fps: 4, no_of_frames: 5, play_frames: 14});
-	}
-});
-function numTwo(){
+if(localStorage.getItem("lvFourStar1")){
+	TweenMax.fromTo(".star-one", 1, {scale:3}, {scale:1});
+	$num_three.setAttribute("class", "invisible");
+	$num_five.setAttribute("class", "number-five");
+}else{
+	$("ul li").mouseup(function(){
+		if ($( "ul li" ).first().hasClass('three')){
+			TweenMax.fromTo(".star-one", 1, {scale:3}, {scale:1});
+			$num_three.setAttribute("class", "invisible");
+			$num_five.setAttribute("class", "number-five");
+			localStorage.setItem("lvFourStar1", 1);
+			//numTwo();
+			$('#iggy-char-lv4').sprite({fps: 4, no_of_frames: 7, play_frames: 14});
+			$('#purple-char-lv4').sprite({fps: 4, no_of_frames: 5, play_frames: 14});
+			$('#neba-char-lv4').sprite({fps: 4, no_of_frames: 5, play_frames: 14});
+		}
+	});
+ }
+//function numTwo(){
+if(localStorage.getItem("lvFourStar2")){
+	TweenMax.fromTo(".star-two", 1, {scale:3}, {scale:1});
+	$num_five.setAttribute("class", "invisible");
+	$num_two.setAttribute("class", "number-two");
+} else{
 	$("ul li").mouseup(function(){
 		if ($( "ul li" ).first().hasClass('five')){
 			TweenMax.fromTo(".star-two", 1, {scale:3}, {scale:1});
 			$num_five.setAttribute("class", "invisible");
 			$num_two.setAttribute("class", "number-two");
-			numThree();
+			//numThree();
+			localStorage.setItem("lvFourStar2", 2);
 			$('#iggy-char-lv4').sprite({fps: 4, no_of_frames: 7, play_frames: 14});
 			$('#purple-char-lv4').sprite({fps: 4, no_of_frames: 5, play_frames: 14});
 			$('#neba-char-lv4').sprite({fps: 4, no_of_frames: 5, play_frames: 14});
 		}
 	});
-}
-function numThree(){
+ }
+//}
+//function numThree(){
+if(localStorage.getItem("lvFourStar3")){ //return;
+	TweenMax.fromTo(".star-three", 1, {scale:3}, {scale:1});
+	$num_two.setAttribute("class", "invisible");
+	$num_one.setAttribute("class", "number-one");
+}else{
 	$("ul li").mouseup(function(){
 		if ($( "ul li" ).first().hasClass('two')){
 			TweenMax.fromTo(".star-three", 1, {scale:3}, {scale:1});
 			$num_two.setAttribute("class", "invisible");
 			$num_one.setAttribute("class", "number-one");
-			numFour();
+			localStorage.setItem("lvFourStar3", 3);
+			//numFour();
 			$('#iggy-char-lv4').sprite({fps: 4, no_of_frames: 7, play_frames: 14});
 			$('#purple-char-lv4').sprite({fps: 4, no_of_frames: 5, play_frames: 14});
 			$('#neba-char-lv4').sprite({fps: 4, no_of_frames: 5, play_frames: 14});
 		}
 	});
-}
-function numFour(){
+	}
+//}
+//function numFour(){
+if(localStorage.getItem("lvFourStar4")){
+	TweenMax.fromTo(".star-four", 1, {scale:3}, {scale:1});
+	$num_one.setAttribute("class", "invisible");
+	$num_four.setAttribute("class", "number-four");
+}else{
 	$("ul li").mouseup(function(){
 		if ($( "ul li" ).first().hasClass('one')){
 			TweenMax.fromTo(".star-four", 1, {scale:3}, {scale:1});
 			$num_one.setAttribute("class", "invisible");
 			$num_four.setAttribute("class", "number-four");
-			numFive();
+			localStorage.setItem("lvFourStar4", 4);
+			//numFive();
 			$('#iggy-char-lv4').sprite({fps: 4, no_of_frames: 7, play_frames: 14});
 			$('#purple-char-lv4').sprite({fps: 4, no_of_frames: 5, play_frames: 14});
 			$('#neba-char-lv4').sprite({fps: 4, no_of_frames: 5, play_frames: 14});
 		}
-
 	});
 }
-function numFive(){
+//}
+// function numFive(){
+if(localStorage.getItem("lvFourStar5")){
+	TweenMax.fromTo(".star-five", 1, {scale:3}, {scale:1});
+	$num_four.setAttribute("class", "invisible");
+	// localStorage.clear();
+}else{
 	$("ul li").mouseup(function(){
 		if ($( "ul li" ).first().hasClass('four')){
 			TweenMax.fromTo(".star-five", 1, {scale:3}, {scale:1});
 			$num_four.setAttribute("class", "invisible");
+			localStorage.setItem("lvFourStar5", 5);
 			$('#iggy-char-lv4').sprite({fps: 4, no_of_frames: 7, play_frames: 14});
 			$('#purple-char-lv4').sprite({fps: 4, no_of_frames: 5, play_frames: 14});
 			$('#neba-char-lv4').sprite({fps: 4, no_of_frames: 5, play_frames: 14});
@@ -92,3 +124,4 @@ function numFive(){
 		}
 	});
 }
+//}
